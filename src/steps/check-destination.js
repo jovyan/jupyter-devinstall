401f2708f40dff690d21657623dd290893191667
Jonathan Frederic <jon.freder@gmail.com>
2015-10-23 15:21:02 -0700

    More bug fixes

--- src/steps/check-destination.js
+++ src/steps/check-destination.js
@@ -4,7 +4,7 @@ import {doesntExist} from '../fs';
 import {StepBase} from './base';
 
 export default class CheckDestination extends StepBase {
-    
+
     /**
      * Public constructor
      *
@@ -16,7 +16,7 @@ export default class CheckDestination extends StepBase {
         super(globals);
         globals.option('-o, --overwrite', 'overwrite existing directories');
     }
-    
+
     /**
      * Gets the name of the section
      * @return {string}
@@ -24,7 +24,7 @@ export default class CheckDestination extends StepBase {
     get section() {
         return "Checking destination";
     }
-    
+
     /**
      * Checks if the step should run
      * @param  {object} previousStepResults - results from the previous step
@@ -33,31 +33,36 @@ export default class CheckDestination extends StepBase {
     shouldRun() {
         return !(this.globals.overwrite || this.globals.reinstall);
     }
-    
+
     /**
      * Run the step
      * @param  {object} previousStepResults - results from the previous step
      * @return {Promise|object} results
      */
     run(previousStepResults) {
-        
+
         // Check if any of the org directories already exist.
         return Promise.all(this.globals.orgs.map(org => {
             return doesntExist(path.resolve(this.globals.installdir, org)).then(() => {
                 this.success(org + ' doesn\'t exist yet');
             }).catch(err => {
-                
-                // The org exists, see if the user would like to overwrite it.
-                return this.yesNo('ipython and/or jupyter and/or jovyan already exist in the destination, overwrite them?').then(response => {
+                return org;
+            });
+        })).then(orgs => {
+            orgs = orgs.filter(x => x !== undefined);
+            if (orgs.length === 0) {
+                return previousStepResults;
+            } else {
+                return this.yesNo('The directories ' + orgs.join(', ') + ' already exist in the destination, overwrite them? ').then(response => {
                     if (response) {
-                        this.success('overwriting ' + org);
+                        this.success('overwriting ' + orgs);
                         this.globals.overwrite = true;
                         return previousStepResults;
                     } else {
                         this.abort('User abort', 0);
                     }
                 });
-            });
-        }));
+            }
+        });
     }
 }
